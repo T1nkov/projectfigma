b281d573317b0293d81062a265d9b6db0824e4fb
T1nkov <mrfredyon@gmail.com>
2022-04-13 14:52:49 +0300

    projectfigma

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+const openPopUp = document.getElementById('openPopUp')
+const closePopUp = document.getElementById('closePopUp')
+const popUp = document.getElementById('popUp')
+
+
+openPopUp.addEventListener('click', (e) =>{
+    e.preventDefault()
+    popUp.classList.add('active');
+})
+
+closePopUp.addEventListener("click", () =>{
+    popUp.classList.remove('active');
+
+})
